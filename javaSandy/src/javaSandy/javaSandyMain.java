b79be5d489378b9f2a2d0d1cbbcab5a5befbd4f2
Johnny Sloans <jsloans2001@yahoo.com>
2017-04-21 16:04:08 -0500

    assg 2 5392

--- javaSandy/src/javaSandy/javaSandyMain.java
+++ javaSandy/src/javaSandy/javaSandyMain.java
@@ -3,8 +3,41 @@ package javaSandy;
 public class javaSandyMain {
 
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-	}
+	public static void main( String[] args ) {
+	      boolean p, q;
+
+	      // Part 1: Exercise the conditional operators AND and OR .....
+
+	      System.out.println( "Part 1: Exercise conditional AND and OR operators");
+	      System.out.println( "=========================================");
+	      System.out.println( "       p        q     (p ^ q)     (p v q)" );
+	      System.out.println( "=========================================");
+
+	      p =  true; q = true;
+	      System.out.printf(  "%8s %8s %11s %11s\n", p, q, p && q, p || q );
+	      p = false; q = true;
+	      System.out.printf(  "%8s %8s %11s %11s\n", p, q, p && q, p || q );
+	      p =  true; q = false;
+	      System.out.printf(  "%8s %8s %11s %11s\n", p, q, p && q, p || q );
+	      p = false; q = false;
+	      System.out.printf(  "%8s %8s %11s %11s\n", p, q, p && q, p || q );
+
+	      System.out.println( "==========================================");
+
+	      // Part 2: the conditional OR operator (i.e., ||)
+
+	      System.out.println( "");
+	      System.out.println( "Part 2: Exercise logical negation operator");
+	      System.out.println( "=========================================");
+	      System.out.println( "       p       !p                        " );
+	      System.out.println( "=========================================");
+
+	      p = true; 
+	      System.out.printf(  "%8s %8s \n", p, !p );
+	      p = false; 
+	      System.out.printf(  "%8s %8s \n", p, !p );
+
+	      System.out.println( "=========================================");
+
+	   } 
 }
