f1ea2afccd02327f65edfd45073d0cf579195865
Johnny <jsloans2001@yahoo.com>
2018-04-13 12:02:08 -0500

    combined with working example

---
+++ javaSandy/src/com/how2examples/lambda/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Evaluates lambda calculus expressions.
+ * <p>
+ * To run from the command line see: {@link com.how2examples.lambda.LambdaCalculusConsole}.
+ * </p>
+ */
+package com.how2examples.lambda;
